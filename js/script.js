2f81ef56ac026c303b789f946566d76722122aae
Slava <vwww@yandex.ru>
2020-12-09 22:16:55 +0300

    FirstCode

--- js/script.js
+++ js/script.js
@@ -23,5 +23,25 @@
 //       chek2 = false;
 //       console.log(chek&&!chek2);
 
-console.log(2+2 === '4');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посморели?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+      
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
+
+      console.log(personalMovieDB);
+
+
+
 
