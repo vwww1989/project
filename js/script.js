de10bcbbac69527df889754b3449255e94806e4b
Slava <vwww@yandex.ru>
2020-12-09 19:25:16 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,27 @@
+"use sctrict";
+// const result = confirm("are you hear?");
+// console.log (result);
+
+// const answer = +prompt('Вам есть 18?', '18');
+// console.log(answer+5);
+
+
+// const answers = []
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваше фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+// document.write(answers);
+
+// const category = 'toys';
+// console.log(`http://yandex.com/${category}/5`);
+
+// const user = 'Petr'
+// alert(`Привет,${user}`);
+
+// const chek = true,
+//       chek2 = false;
+//       console.log(chek&&!chek2);
+
+console.log(2+2 === '4');
+
