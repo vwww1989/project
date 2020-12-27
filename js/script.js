512c75b60a86611da8bd76c68a6f2e3df483181e
Slava <vwww@yandex.ru>
2020-12-27 21:07:19 +0300

    '1'

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@
 // const chek = true,
 //       chek2 = false;
 //       console.log(chek&&!chek2);
-
+ 
 const numberOfFilms = +prompt('Сколько фильмов вы уже посморели?', '');
 const personalMovieDB = {
     count: numberOfFilms,
@@ -32,15 +32,43 @@ const personalMovieDB = {
     privat: false,
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
-      
-      personalMovieDB.movies[a] = b;
-      personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+     const a = prompt('Один из последних просмотренных фильмов?', ''),
+           b = prompt('На сколько оцените его?', '');
+     
 
-      console.log(personalMovieDB);
+     if (a !=null && b !=null && a !='' && b !='' && a.length <50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+        
+     }
+     else {
+         console.log('error');
+         i--;
+     }
+
+ }
+
+   if (personalMovieDB.count <10) {
+       console.log('Просмотренно довольно мало фильмов');
+
+   }
+   else if (personalMovieDB.count >=10 && personalMovieDB.count <30) {
+       console.log('Вы класический зритель');
+   }
+   
+else if (personalMovieDB.count >=30) {
+    console.log('Вы киноман');
+
+}
+
+else {
+    console.log ('Произошла ошибка');
+
+}
+
+
+
 
 
 
